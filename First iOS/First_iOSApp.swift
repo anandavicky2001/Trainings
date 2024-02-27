f7df57cce7a241fa0ff2658681c10c46c063fc9a
Ananda Vicky <anandavikisns@gmail.com>
2024-02-27 14:15:42 +0700

    Initial Commit

---
+++ First iOS/First_iOSApp.swift
@@ -0,0 +1,32 @@
+//
+//  First_iOSApp.swift
+//  First iOS
+//
+//  Created by XcodeTraining on 27/02/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct First_iOSApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ First iOS/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  First iOS
+//
+//  Created by XcodeTraining on 27/02/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
